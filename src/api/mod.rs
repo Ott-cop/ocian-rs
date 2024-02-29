08b20df37070ccd64682bee79b737fd9edda3ced
Ott-cop <otavio.onoue@outlook.com>
2024-02-29 03:00:46 -0300

    Organize the code

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,5 @@
-use std::env::{self, temp_dir};
-use std::fs;
+use std::env::{self};
+use std::io::Read;
 use actix_multipart::form::MultipartForm;
 use actix_multipart::form::{tempfile::TempFile, text::Text};
 use actix_web::{web, HttpResponse, Responder};
@@ -7,8 +7,6 @@ use lettre::message::header::{self, ContentType};
 use lettre::message::{Attachment, Mailbox, MultiPart, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
-use uuid::Uuid;
-
 
 use crate::AppState;
 
@@ -36,7 +34,8 @@ struct Error<'a> {
     error: Vec<&'a str>
 }
 
-fn validate(name: &String, email: &String, phone: &String, subject: Option<&String>, message: &String) -> Result<(), Error<'static>> {
+
+fn validate(name: &String, email: &String, phone: &String, subject: Option<&String>, message: &String, file: Option<&TempFile>) -> Result<(), Error<'static>> {
 
     let mut error: Vec<&str> = vec![];
 
@@ -52,6 +51,12 @@ fn validate(name: &String, email: &String, phone: &String, subject: Option<&Stri
         }
     } if message.len() < 6 || message.len() > 500 {
         error.push("message");
+    } if file.is_some() {
+        let file = file.unwrap();
+
+        if file.file_name.is_none() {
+            error.push("file");
+        } 
     }
 
     let error_json = Error {
@@ -66,88 +71,15 @@ fn validate(name: &String, email: &String, phone: &String, subject: Option<&Stri
     Ok(())
 }
 
-pub async fn send_proposal(app_state: web::Data<AppState>, req: web::Json<User>) -> impl Responder {
-
-    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message) { 
-        return HttpResponse::BadRequest().json(err);
-    } 
-    
-    match sqlx::query("INSERT INTO proposal (name, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5)")
-    .bind(&req.name)
-    .bind(&req.email)
-    .bind(&req.phone)
-    .bind(&req.subject)
-    .bind(&req.message)
-    .execute(&app_state.pool).await {
-        Ok(_) => HttpResponse::Ok().json("The request has been sended!"),
-        Err(err) => HttpResponse::BadRequest().json(err.to_string())
-    }
-}
-
-pub async fn send_contact_us(app_state: web::Data<AppState>, req: web::Json<User>) -> impl Responder {
-
-    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message) { 
-        return HttpResponse::BadRequest().json(err);
-    } 
-
-    match sqlx::query("INSERT INTO contact_us (name, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5)")
-    .bind(&req.name)
-    .bind(&req.email)
-    .bind(&req.phone)
-    .bind(&req.subject)
-    .bind(&req.message)
-    .execute(&app_state.pool).await {
-        Ok(_) => HttpResponse::Ok().json("The request has been sended!"),
-        Err(err) => HttpResponse::BadRequest().json(err.to_string())
-    }
-}
-
-pub async fn send_work_with_us(MultipartForm(form): MultipartForm<Curriculum>) -> impl Responder {
-    let from_email: String = env::var("FROM_EMAIL").expect("FROM_EMAIL not found!");
-    let host: String = env::var("HOST").expect("HOST not found!");
-    let to_email = env::var("TO_EMAIL").expect("TO_EMAIL not found!");
-    let username = env::var("USERNAME").expect("USERNAME not found!");
-    let password = env::var("PASSWORD").expect("PASSWORD not found!");
-
-    if let Err(err) = validate(&form.name.0, &form.email.0, &form.phone.0, None, &form.message.0) {
-        return HttpResponse::BadRequest().json(err);
-    }
-
-    if form.file.file_name.is_none() {
-        return HttpResponse::BadRequest().json("CV is required");
-    }
-
-    let f = form.file;
-
-    let filename = format!("{}-{}-{}", form.name.0, Uuid::new_v4(), f.file_name.clone().unwrap());
-
-    let path = format!("{}/temp_curriculum/{}", temp_dir().display(), filename);
-
-    f.file.persist(path.clone()).unwrap();
-
-    let ftype = f.content_type.unwrap().to_string();
-
-    let type_value: Result<String, HttpResponse> = match ftype {
-        ftype if ftype == String::from("application/pdf") => Ok(String::from("application/pdf")),
-        ftype if ftype == String::from("application/msword") => Ok(String::from("application/msword")),
-        ftype if ftype == String::from("application/vnd.openxmlformats-officedocument.wordprocessingml.document") => Ok(String::from("application/vnd.openxmlformats-officedocument.wordprocessingml.document")),
-        _ => Err(HttpResponse::BadRequest().json("[!] The file format is invalid."))
-    };
-    
-    if let Err(err) = type_value {
-        return err;
-    }
-
-    let type_value = type_value.unwrap();
-
-    let filebody = fs::read(path.clone()).unwrap();
+fn make_email(host: String, username: String, password: String, from_email: String, to_email: String, name: &String, email: &String, phone: &String, message: &String, filebody: Vec<u8>, filename: String, type_value: String) -> (SmtpTransport, Message) {
+  
 
     let content_type = ContentType::parse(type_value.as_str()).unwrap();
 
     let attachment = Attachment::new(filename).body(filebody, content_type);
 
 
-    let body = format!("\n{}\n{}\n{}\n\n{}", form.name.0, form.email.0, form.phone.0, form.message.0);
+    let body = format!("\nNome: {}\nEmail: {}\nTelefone: {}\n\nMensagem: {}", name, email, phone, message);
     let message = Message::builder()
         .from(from_email.parse::<Mailbox>().unwrap())
         .to(to_email.parse::<Mailbox>().unwrap())
@@ -166,19 +98,81 @@ pub async fn send_work_with_us(MultipartForm(form): MultipartForm<Curriculum>) -
         .port(587)
         .build();
 
+    (mailer, message)
+}
+
+pub async fn send_proposal(app_state: web::Data<AppState>, req: web::Json<User>) -> impl Responder {
+    
+    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message, None) { 
+        return HttpResponse::BadRequest().json(err);
+    } 
+    
+    match sqlx::query("INSERT INTO proposal (name, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5)")
+    .bind(&req.name)
+    .bind(&req.email)
+    .bind(&req.phone)
+    .bind(&req.subject)
+    .bind(&req.message)
+    .execute(&app_state.pool).await {
+        Ok(_) => HttpResponse::Ok().json("The request has been sended!"),
+        Err(err) => HttpResponse::BadRequest().json(err.to_string())
+    }
+}
+
+pub async fn send_contact_us(app_state: web::Data<AppState>, req: web::Json<User>) -> impl Responder {
+
+    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message, None) { 
+        return HttpResponse::BadRequest().json(err);
+    } 
+
+    match sqlx::query("INSERT INTO contact_us (name, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5)")
+    .bind(&req.name)
+    .bind(&req.email)
+    .bind(&req.phone)
+    .bind(&req.subject)
+    .bind(&req.message)
+    .execute(&app_state.pool).await {
+        Ok(_) => HttpResponse::Ok().json("The request has been sended!"),
+        Err(err) => HttpResponse::BadRequest().json(err.to_string())
+    }
+}
+
+pub async fn send_work_with_us(MultipartForm(mut form): MultipartForm<Curriculum>) -> impl Responder {
+    let from_email: String = env::var("FROM_EMAIL").expect("FROM_EMAIL not found!");
+    let host: String = env::var("HOST").expect("HOST not found!");
+    let to_email = env::var("TO_EMAIL").expect("TO_EMAIL not found!");
+    let username = env::var("USERNAME").expect("USERNAME not found!");
+    let password = env::var("PASSWORD").expect("PASSWORD not found!");
+
+    if let Err(err) = validate(&form.name.0, &form.email.0, &form.phone.0, None, &form.message.0, Some(&form.file)) {
+        return HttpResponse::BadRequest().json(err);
+    }
+    let mut file: Vec<u8> = vec![];
+    let _ = form.file.file.read_to_end(&mut file);
+    
+    let type_value: Result<String, HttpResponse> = match form.file.content_type.unwrap().to_string() {
+        ftype if ftype == String::from("application/pdf") => Ok(String::from("application/pdf")),
+        ftype if ftype == String::from("application/msword") => Ok(String::from("application/msword")),
+        ftype if ftype == String::from("application/vnd.openxmlformats-officedocument.wordprocessingml.document") => Ok(String::from("application/vnd.openxmlformats-officedocument.wordprocessingml.document")),
+        _ => Err(HttpResponse::BadRequest().json("The file format is invalid."))
+    };
+
+    if let Err(err) = type_value {
+        return err;
+    }
+    
+    let (mailer, message) = make_email(host, username, password, from_email, to_email, &form.name.0, &form.email.0, &form.phone.0, &form.message.0, file, form.file.file_name.unwrap(), type_value.unwrap());
+
     match mailer.send(&message) {
-        Ok(_) => HttpResponse::Ok().json("Caguei pro c"),
-        Err(err) => {
-            println!("O erro é o seguinte: {}", err);
-            HttpResponse::BadGateway().json(err.to_string())
-        }
+        Ok(_) => HttpResponse::Ok().json("The message has been sended!"),
+        Err(err) => HttpResponse::BadGateway().json(err.to_string())
     }
     
 }
 
 pub async fn send_support(app_state: web::Data<AppState>, req: web::Json<User>) -> impl Responder {
     
-    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message) { 
+    if let Err(err) = validate(&req.name, &req.email, &req.phone, Some(&req.subject), &req.message, None) { 
         return HttpResponse::BadRequest().json(err);
     } 
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_cors::Cors;
 use actix_web::{http::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE}, web::{self}, App, HttpServer};
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use std::env::{self, temp_dir, VarError};
+use std::env::{self, VarError};
 
 mod api;
 use api::{send_contact_us, send_proposal, send_support, send_work_with_us};
@@ -14,10 +14,12 @@ struct AppState {
 #[actix_web::main]
 async fn main() {
     dotenv::dotenv().ok();
+    
 
     let db_url: Result<String, VarError> = env::var("DATABASE_URL");
 
     let pool = PgPoolOptions::new().connect(db_url.unwrap().as_str()).await;
+
     match &pool {
         Ok(_) => println!("[+] Stabilized connection to the server!"),
         Err(err) => println!("[!] Error found: {}", err)
@@ -25,14 +27,6 @@ async fn main() {
     let pool = pool.unwrap();
 
     let _ = sqlx::migrate!("./migrations").run(&pool).await; 
-
-    let app_state = AppState { pool: pool.clone() };
-
-    let _ = std::process::Command::new("mkdir")
-        .arg(format!("{}/temp_curriculum", temp_dir().display()))
-        .spawn()
-        .expect("[!] Existing folder...")
-        .wait();
     
     HttpServer::new(move || {
         let cors = Cors::default()
@@ -42,7 +36,7 @@ async fn main() {
             .allowed_methods(vec!["POST"])
             .max_age(3600);
         App::new()
-        .app_data(web::Data::new(app_state.clone()))
+        .app_data(web::Data::new(AppState { pool: pool.clone() }))
             .wrap(cors)
             .route("/send_proposal", web::post().to(send_proposal))
             .route("/send_contact_us", web::post().to(send_contact_us))
